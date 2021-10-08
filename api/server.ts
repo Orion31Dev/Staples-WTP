2434e9a0bb5c08cdc41602a5ee8ed99a9b4ba576
Ryan <orion31dev@gmail.com>
2021-10-08 19:45:16 -0400

    Add /unit-data endpoint to api

--- api/server.ts
+++ api/server.ts
@@ -8,5 +8,13 @@ if (process.env.NODE_ENV !== 'production') {
 const Cloudant = require('@cloudant/cloudant');
 const cloudant = new Cloudant({ url: process.env.CLOUDANT_URL, plugins: { iamauth: { iamApiKey: process.env.CLOUDANT_API_KEY } } });
 
+const db = cloudant.use('wtp');
+
 const app = express();
 app.use(express.json());
+
+app.get('/unit-data', async (req, res) => {
+  res.json(await db.get('unit_data'));
+});
+
+app.listen(process.env.PORT || 3001);
