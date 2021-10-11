377ca11568ba43bfedebd36366da155e3bdefcc7
Ryan <orion31dev@gmail.com>
2021-10-10 23:37:27 -0400

    oops again (literally what??)

--- client/src/components/unit/UnitData.ts
+++ client/src/components/unit/UnitData.ts
@@ -25,8 +25,8 @@ export interface IVideo {
 }
 
 export async function getUnitData() {
-  let data = await fetch('http://localhost:3001/api/unit-data');
-  // let data = await fetch('https://shs-wtp.vercel.app/api/unit-data').then(res => res.json());
+  //let data = await fetch('http://localhost:3001/api/unit-data');
+  let data = await fetch('https://shs-wtp.vercel.app/api/unit-data').then(res => res.json());
 
   return await data.json();
 }
