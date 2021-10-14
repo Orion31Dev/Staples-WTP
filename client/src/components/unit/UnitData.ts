8046f9f3393c4bf5696c55d4c535267b93b7a8bf
Ryan <orion31dev@gmail.com>
2021-10-13 20:20:05 -0400

    fix getUnitData

--- client/src/components/unit/UnitData.ts
+++ client/src/components/unit/UnitData.ts
@@ -1,6 +1,6 @@
 export async function getUnitData() {
-  let data = await fetch('http://localhost:3001/api/unit-data');
-  //let data = await fetch('https://shs-wtp.vercel.app/api/unit-data');
+  //let data = await fetch('http://localhost:3001/api/unit-data');
+  let data = await fetch('https://shs-wtp.vercel.app/api/unit-data');
 
   return await data.json();
 }
