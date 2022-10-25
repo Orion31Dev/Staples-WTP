1965740c2d50c70aa142c259ac153385beaaa8cd
Ryan Salik <rssalik14@gmail.com>
2022-10-25 14:00:53 -0400

    Update tests.ts

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
 test('Six unit links appear', async ({ page }) => {
-	await page.goto('/units');
+	await page.goto('/units', { waitUntil: 'domcontentloaded' });
 	expect(await page.textContent('.unit-links')).toHaveLength(6);
 });
