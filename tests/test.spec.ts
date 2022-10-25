7b0f90fda49d27c5ee7e64660bfbfab2b31d1d16
Ryan Salik <rssalik14@gmail.com>
2022-10-25 15:05:14 -0400

    please

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Six unit links appear', async ({ page }) => {
-	await page.goto('http://localhost:4173/units', { waitUntil: 'domcontentloaded' });
+	await page.goto('/units', { waitUntil: 'domcontentloaded' });
 	await expect(page.locator('.unit-link')).toHaveCount(6);
 
 });
