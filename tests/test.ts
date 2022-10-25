36eaa1ecdce20c671eb3e8588ddf10de0d9c7a5f
Ryan Salik <rssalik14@gmail.com>
2022-10-25 14:43:50 -0400

    ugh pt. 2

--- tests/test.ts
+++ tests/test.ts
@@ -2,6 +2,6 @@ import { expect, test } from '@playwright/test';
 
 test('Six unit links appear', async ({ page }) => {
 	await page.goto('/units', { waitUntil: 'domcontentloaded' });
-	console.log(page.title());
+	console.log(await page.title());
 	expect(await page.locator('.unit-link').count()).toBe(6);
 });
